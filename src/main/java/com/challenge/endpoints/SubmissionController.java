68da62bbc83ebfcb255529c7d2df99b8f4d05bf9
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-08-03 22:01:36 -0300

    DTOsubmiss

--- src/main/java/com/challenge/endpoints/SubmissionController.java
+++ src/main/java/com/challenge/endpoints/SubmissionController.java
@@ -1,7 +1,10 @@
 package com.challenge.endpoints;
 
 
+import com.challenge.dto.SubmissionDTO;
 import com.challenge.entity.Submission;
+
+import com.challenge.mappers.SubmissionMapper;
 import com.challenge.service.impl.SubmissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,14 +17,22 @@ public class SubmissionController {
 
     @Autowired private SubmissionService submissionService;
 
+    @Autowired private SubmissionMapper submissionMapper;
+
+
+
     @GetMapping
-    public ResponseEntity<List<Submission>> findbyChallengeIdAndAccelerationId(
-            @RequestParam Long challengeId,
-            @RequestParam Long accelerationId){
+    public ResponseEntity<List<SubmissionDTO>> findbyChallengeIdAndAccelerationId(
+            @RequestParam(required = true,name = "challengeId") Long challengeId,
+            @RequestParam (required = true,name="accelerationId")Long accelerationId){
+
         List<Submission> sbmssns = null;
         sbmssns=submissionService.findByChallengeIdAndAccelerationId(challengeId,accelerationId);
+
+
         return sbmssns.isEmpty()?
-                ResponseEntity.notFound().build():ResponseEntity.ok(sbmssns);
+                ResponseEntity.notFound().build():
+                ResponseEntity.ok(submissionMapper.map(sbmssns));
     }
 
 }
